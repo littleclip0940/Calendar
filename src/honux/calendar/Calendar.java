2b4e25a4ab5bf770db94bb0af48949a7090d4a4e
user <user@LAPTOP-N426AL6N>
2018-09-01 23:45:00 +0900

    class 활용하여 달의 max days 출력

--- src/honux/calendar/Calendar.java
+++ src/honux/calendar/Calendar.java
@@ -1,16 +1,57 @@
 package honux.calendar;
 
-public class Calendar {
+import java.util.Scanner;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+
+public class Calendar {
+	
+	
+	public static final int[] Month= {31,28,31,30,31,30,31,31,30,31,30,31};
+	
+	public void printSampleCalendar() {
+		
+		
 		System.out.println("일 월 화 수 목 금 토");
 		System.out.println("--------------------");
 		System.out.println("1  2  3  4  5  6  7");
 		System.out.println("8  9 10 11 12 13 14");
 		System.out.println("15 16 17 18 19 20 21");
 		System.out.println("22 23 24 25 26 27 28"); 
+		
+		
+	}
+	
+	
+	public int getMaxDaysOfMonth(int month) {
+		
+		return Month[month-1];
+		
+		
+	}
+	
+
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		Scanner scan=new Scanner(System.in);
+		Calendar cal=new Calendar();
+		
+		int input;
+		
+		System.out.println("알고 싶은 달을 하나 선택하세요(1월~12월 중): ");
+		
+		String month_s=scan.next();
+		
+		input=Integer.parseInt(month_s);		
+		
+		System.out.printf("%d월은 %d일까지 있습니다.", input, cal.getMaxDaysOfMonth(input) );
+		
+		scan.close();
 
 	}
-
 }
